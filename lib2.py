12596cfd5bcf11bfb4df14b99ead48d618bebd1b
Alexander Braun <alex-n-braun@users.noreply.github.com>
2017-03-17 23:29:13 +0100

    well...

--- lib2.py
+++ lib2.py
@@ -1,5 +1,16 @@
 import numpy as np
 
+def rectangle(polygon):
+    xMin=polygon[0][1]; xMax=xMin
+    yMin=polygon[0][0]; yMax=yMin
+    for i in range(1, len(polygon)):
+        xMin=xMin if xMin<=polygon[i][0] else polygon[i][0]
+        xMax=xMax if xMax>=polygon[i][0] else polygon[i][0]
+        yMin=yMin if yMin<=polygon[i][1] else polygon[i][1]
+        yMax=yMax if yMax>=polygon[i][1] else polygon[i][1]
+        
+    return [[xMin, yMin],[xMax, yMax]]
+
 # given a polygon (with exactly 4 corners!!!), cut a
 # rectangular area from img that contains the complete polygon.
 def region_of_interest(img, polygon):
@@ -51,3 +62,71 @@ def top_boundary(groundWindow=[[0,279],[240,50],[800,50],[1003,70], [1003,279]])
 
 def bottom_boundary(groundWindow=[[0,279],[240,50],[800,50],[1003,70], [1003,279]]):
     return max(np.array(groundWindow)[:,1])
+
+# Source of the code: Udacity, Self-Driving Car Engineer. Adapted to support x range as well.
+# Define a single function that can extract features using hog sub-sampling and make predictions
+def find_cars2(img, xstart, xstop, ystart, ystop, scale, 
+               svc, X_scaler, orient, pix_per_cell, cell_per_block, cells_per_step_x, cells_per_step_y, 
+               spatial_size, hist_bins):
+        
+    img_tosearch = img[ystart:ystop,xstart:xstop,:]
+    ctrans_tosearch = convert_color(img_tosearch, conv='RGB2YCrCb')
+    if scale != 1:
+        imshape = ctrans_tosearch.shape
+        ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1]/scale), np.int(imshape[0]/scale)))
+        
+    ch1 = ctrans_tosearch[:,:,0]
+    ch2 = ctrans_tosearch[:,:,1]
+    ch3 = ctrans_tosearch[:,:,2]
+
+    # Define blocks and steps as above
+    nxblocks = (ch1.shape[1] // pix_per_cell)-1
+    nyblocks = (ch1.shape[0] // pix_per_cell)-1 
+    nfeat_per_block = orient*cell_per_block**2
+    # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
+    window = 64
+    nblocks_per_window = (window // pix_per_cell)-1 
+    
+    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step_x + 1
+    nysteps = (nyblocks - nblocks_per_window) // cells_per_step_y + 1
+    
+    # Compute individual channel HOG features for the entire image
+    hog1 = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    
+    boxes=[]
+    
+    for xb in range(nxsteps):
+        for yb in range(nysteps):
+            ypos = yb*cells_per_step_y
+            xpos = xb*cells_per_step_x
+            # Extract HOG for this patch
+            hog_feat1 = hog1[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
+            hog_feat2 = hog2[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
+            hog_feat3 = hog3[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
+            hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3))
+
+            xleft = xpos*pix_per_cell
+            ytop = ypos*pix_per_cell
+
+            # Extract the image patch
+            subimg = cv2.resize(ctrans_tosearch[ytop:ytop+window, xleft:xleft+window], (64,64))
+          
+            # Get color features
+            spatial_features = bin_spatial(subimg, size=spatial_size)
+            hist_features = color_hist(subimg, nbins=hist_bins)
+
+            # Scale features and make a prediction
+            test_features = X_scaler.transform(np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))    
+            #test_features = X_scaler.transform(np.hstack((shape_feat, hist_feat)).reshape(1, -1))    
+            test_prediction = svc.predict(test_features)
+            
+            if test_prediction == 1:
+                xbox_left = np.int(xleft*scale)
+                ytop_draw = np.int(ytop*scale)
+                win_draw = np.int(window*scale)
+                box=((xbox_left+xstart, ytop_draw+ystart), (xbox_left+win_draw+xstart,ytop_draw+win_draw+ystart))
+                boxes.append(box)
+                
+    return boxes
